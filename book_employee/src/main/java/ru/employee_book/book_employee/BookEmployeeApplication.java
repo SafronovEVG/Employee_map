f3037ff7ea37b0d3b9b671f8aef3d880dd733b1c
Евгений <Safronovbr@mail.ru>
2024-11-20 14:20:01 +0300

    Start

---
+++ book_employee/src/main/java/ru/employee_book/book_employee/BookEmployeeApplication.java
@@ -0,0 +1,13 @@
+package ru.employee_book.book_employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookEmployeeApplication.class, args);
+	}
+
+}
---
+++ book_employee/src/test/java/ru/employee_book/book_employee/BookEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.employee_book.book_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
